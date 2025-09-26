8c084e7435a6613f835923988ca418e26263f72c
lennykombo <kombolenny1@gmail.com>
2025-09-26 13:04:11 +0300

    change an import

--- netlify/functions/sendWhatsApp.js
+++ netlify/functions/sendWhatsApp.js
@@ -1,4 +1,3 @@
-import fetch from "node-fetch";
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import "dotenv/config";
